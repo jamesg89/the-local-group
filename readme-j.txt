b6e30a66d0cade1a1941d7c921ce8605bea1979f
jamesg89 <90154865+jamesg89@users.noreply.github.com>
2024-10-02 20:41:43 -0700

    Nav Bar onScroll logic added

---
+++ readme-j.txt
@@ -0,0 +1,2 @@
+npx kill-port 5173
+npx kill-port 9999 to close server
--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,22 +1,68 @@
 <script>
-    import { PrismicLink } from "@prismicio/svelte";
-    // @ts-ignore
-    import WordMark from "./WordMark.svelte";
-    /** @type {import("@prismicio/client").Content.SettingsDocument}*/  
-    export let settings;
+  import { PrismicLink } from "@prismicio/svelte";
+  // @ts-ignore
+  import WordMark from "./WordMark.svelte";
+  /** @type {import("@prismicio/client").Content.SettingsDocument}*/ 
+  
+  import { onMount } from 'svelte';
+  import { onDestroy } from 'svelte';
+
+  let scrolled = false;
+
+  const handleScroll = () => {
+    scrolled = window.scrollY > 40;
+  };
+
+  onMount(() => {
+    if (typeof window !== 'undefined') {
+      window.addEventListener('scroll', handleScroll);
+    }
+  });
+
+  onDestroy(() => {
+    if (typeof window !== 'undefined') {
+      window.removeEventListener('scroll', handleScroll);
+    }
+  });
+
+  export let settings;
 </script>
-<header class="fixed top-0 w-full transparent backdrop-blur-sm duration-300 z-50 transition-transform ease-in">
-    <nav class="container mx-auto flex flex-col md:flex-row justify-between py-4" aria-label="header">
-        <a href="/"><WordMark Clr="#fff" /></a>
-        <span class="sr-only">{settings.data.site_title} home page</span>
-            <ul class="flex gap-6">
-                {#each settings.data.navigation as item (item.label)}
-                    <li class="hover:text-slate text-white">
-                        <PrismicLink field={item.link} class="inline-flex min-h-11 items-center">
-                            {item.label}
-                        </PrismicLink>
-                    </li>
-                {/each}
-            </ul>
-    </nav>
+
+<style>
+  header {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    height: 80px;
+    display: flex;
+    align-items: center;
+    transition: background-color 0.5s ease, border 0.5s ease;
+  }
+
+  header.transparent {
+    background-color: rgba(0, 0, 0, 0.7);
+    border-bottom: 2px black solid;
+  }
+
+  header.default {
+    background-color: transparent;
+    border-bottom: none;
+  }
+</style>
+
+<header class="{scrolled ? 'transparent' : 'default'} fixed top-0 w-full z-50 transparent backdrop-blur-sm duration-300 transition-transform ease-in">
+  <nav class="container mx-auto flex flex-col md:flex-row justify-between py-4" aria-label="header">
+      <a href="/"><WordMark Clr="#fff" /></a>
+      <span class="sr-only">{settings.data.site_title} home page</span>
+          <ul class="flex gap-6">
+              {#each settings.data.navigation as item (item.label)}
+                  <li class="hover:text-slate text-white">
+                      <PrismicLink field={item.link} class="inline-flex min-h-11 items-center">
+                          {item.label}
+                      </PrismicLink>
+                  </li>
+              {/each}
+          </ul>
+  </nav>
 </header>
